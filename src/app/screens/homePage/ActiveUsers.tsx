ebdac6e18a9d9ecd9467bd88a68960da985c269b
valijon1989 <valijon.jon89@gmail.com>
2025-07-29 03:23:47 +0900

    B: feat: develop activeUsers section component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -1,12 +1,42 @@
-import React from "react";
-import { Container } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
+import Card from "@mui/joy/Card";
+import { CssVarsProvider, Typography } from "@mui/joy";
+import CardOverflow from "@mui/joy/CardOverflow";
+import AspectRatio from "@mui/joy/AspectRatio";
+
+const activeUsers = [
+  { memberNick: "Martin", memberImage: "/img/martin.webp" },
+  { memberNick: "Justin", memberImage: "/img/justin.webp" },
+  { memberNick: "Rose", memberImage: "/img/rose.webp" },
+  { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
+];
 
 export default function ActiveUsers() {
-    return (
-        <div>
-            <Container>ActiveUsers</Container>
-        </div>
-    );
+  return (
+    <div className="active-users-frame">
+      <Container>
+        <Stack className="main">
+          <Box className="category-title">Active Users</Box>
+          <Stack className="cards-frame">
+            <CssVarsProvider>
+              {activeUsers.length !== 0 ? (
+                activeUsers.map((user, index) => (
+                  <Card key={index} className="card">
+                    <CardOverflow>
+                      <AspectRatio ratio="1">
+                        <img src={user.memberImage} alt={user.memberNick} />
+                      </AspectRatio>
+                    </CardOverflow>
+                    <Typography level="body-md">{user.memberNick}</Typography>
+                  </Card>
+                ))
+              ) : (
+                <Box className="no-data">No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
-
-
