8feb2a6f989229a316b90c908706bb6a9ed99f52
valijon1989 <valijon.jon89@gmail.com>
2025-08-07 03:16:50 +0900

    B: fix: publish userPage screen component

--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -1,5 +1,5 @@
 import { useState, SyntheticEvent } from "react";
-import { Container, Stack, Box } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import TabContext from "@mui/lab/TabContext";
@@ -8,115 +8,109 @@ import PausedOrders from "./PausedOrders";
 import ProcessOrders from "./ProcessOrders";
 import FinishedOrders from "./FinishedOrders";
 import "../../../css/order.css";
-
+ 
 export default function OrdersPage() {
-  const [value, setValue] = useState("1");
-
-  const handleChange = (e: SyntheticEvent, newValue: string) => {
-    setValue(newValue);
-  };
-
-  return (
-    <div className="order-page">
-      <Container className="order-container">
-        {/* Left side with tabs and orders */}
-        <Stack className="order-left">
-          <TabContext value={value}>
-            <Box className="order-nav-frame">
-              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  aria-label="basic tabs example"
-                  className="table_list"
-                >
-                  <Tab label="PAUSED ORDERS" value="1" />
-                  <Tab label="PROCESS ORDERS" value="2" />
-                  <Tab label="FINISHED ORDERS" value="3" />
-                </Tabs>
-              </Box>
-            </Box>
-
-            <Stack className="order-main-content">
-              <PausedOrders />
-              <ProcessOrders />
-              <FinishedOrders />
-            </Stack>
-          </TabContext>
-        </Stack>
-
-        {/* Right side with user info and card form */}
-        <Stack className="order-right">
-          <Box className="order-info-box">
-            <Box className="member-box">
-              <div className="order-user-img">
-                <img
-                  src="/icons/default-user.svg"
-                  className="order-user-avatar"
-                />
-                <div className="order-user-icon-box">
-                  <img
-                    src="/icons/user-badge.svg"
-                    className="order-user-prof-img"
-                  />
-                </div>
-              </div>
-              <span className="order-user-name">Jon</span>
-              <span className="order-user-prof">User</span>
-            </Box>
-
-            <Box className="liner" />
-
-            <Box className="order-user-address">
-              <div style={{ display: "flex" }}>
-                <LocationOnIcon />
-              </div>
-              <div style={{ display: "flex", flexDirection: "column" }}>
-                <span className="order-user-address-info">Do not exist</span>
-              </div>
-            </Box>
-          </Box>
-
-          <Box className="order-info-box">
-            <Box className="card-number-box">
-              <input
-                type="text"
-                placeholder="Card number: **** 4090 2002 7495"
-                className="card-input"
-              />
-            </Box>
-          
-            <Box className="card-date-box">
-              <input
-                type="text"
-                placeholder="07 / 24"
-                className="card-date-input"
-              />
-              <input
-                type="text"
-                placeholder="CVV : 010"
-                className="card-cvv-input"
-              />
-            </Box>
-           
-
-            <Box className="card-user-box">
-              <input
-                type="text"
-                placeholder="Justin Robertson"
-                className="card-user-input"
-              />
-            </Box>
-
-            <Box className="card-pay-icons">
-              <img src="/icons/mastercard.svg" />
-              <img src="/icons/union.svg" />
-              <img src="/icons/paypal.svg" />
-              <img src="/icons/visa.svg" />
-            </Box>
-          </Box>
-        </Stack>
-      </Container>
-    </div>
-  );
+const [value, setValue] = useState("1");
+ 
+const handleChange = (e: SyntheticEvent, newValue: string) => {
+setValue(newValue);
+};
+ 
+return (
+<div className={"order-page"}>
+<Container className={"order-container"}>
+<Stack className={"order-left"}>
+<TabContext value={value}>
+<Box className={"order-nav-frame"}>
+<Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+<Tabs
+value={value}
+onChange={handleChange}
+aria-label="basic tabs example"
+className={"table-list"}
+>
+<Tab label="PAUSED ORDERS" value={"1"} />
+<Tab label="PROCESS ORDERS" value={"2"} />
+<Tab label="FINISHED ORDERS" value={"3"} />
+</Tabs>
+</Box>
+</Box>
+<Stack className={"order-main-content"}>
+<PausedOrders />
+<ProcessOrders />
+<FinishedOrders />
+</Stack>
+</TabContext>
+</Stack>
+ 
+<Stack className={"order-right"}>
+<Box className={"member-info-boxes"}>
+<Box className={"member-box"}>
+<div className={"order-user-img"}>
+<img
+src={"/icons/default-user.svg"}
+className={"order-user-avatar"}
+/>
+<div className={"order-user-icon-box"}>
+<img
+src={"/icons/user-badge.svg"}
+className={"order-user-prof-img"}
+/>
+</div>
+</div>
+<span className={"order-user-name"}>Martin</span>
+<span className={"order-user-prof"}>User</span>
+<Box className={"liner"}></Box>
+<Box className={"order-user-address"}>
+<div>
+<LocationOnIcon />
+</div>
+<span>South Korea, Busan</span>
+</Box>
+</Box>
+<Box className={"member-card-box"}>
+<Box className={"member-card-inputs"}>
+<div className={"card-number"}>
+<input
+type="text"
+name="cardNumber"
+placeholder="Card number: **** 4090 2002 7465"
+className={"card-number-input"}
+/>
+</div>
+<div className={"card-valid"}>
+<input
+type="text"
+name="cardValDate"
+placeholder="07/28"
+className={"card-val-date"}
+/>
+<input
+type="text"
+name="cardCCV"
+placeholder="CCV: 010"
+className={"card-val-date"}
+/>
+</div>
+<div className={"card-holder"}>
+<input
+type="text"
+name="cardNumber"
+placeholder="Jusin Robertson"
+className={"card-holder-input"}
+/>
+</div>
+</Box>
+<Box className={"member-card-type"}>
+<img src="/icons/western-card.svg" alt="" />
+<img src="/icons/master-card.svg" alt="" />
+<img src="/icons/paypal-card.svg" alt="" />
+<img src="/icons/visa-card.svg" alt="" />
+</Box>
+</Box>
+</Box>
+</Stack>
+</Container>
+</div>
+);
 }
