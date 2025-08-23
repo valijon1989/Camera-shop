d698f48af8b42e09fd1ae1b938fb7e8ee183e610
valijon1989 <valijon.jon89@gmail.com>
2025-08-24 02:44:02 +0900

    B: feat: develop AppRootState and HomePage type integrations

---
+++ src/lib/enums/member.enum.ts
@@ -0,0 +1,13 @@
+export enum MemberType {
+    USER = "USER",
+    RESTAURANT = "RESTAURANT",
+}
+
+export enum MemberStatus {
+    ACTIVE = "ACTIVE",
+    BLOCK = "BLOCK",
+    DELETE = "DELETE",
+}
+
+
+
---
+++ src/lib/types/screen.ts
@@ -0,0 +1,22 @@
+
+import { Member } from "./member";
+import { Product } from "./product";
+
+/*. REACT APP STATE */
+export interface AppROOTState {
+    homePage: HomePageState;
+}
+
+
+
+/*. HOME PAGE STATE */
+export interface HomePageState {
+popularDishes: Product[];
+newDishes: Product[];
+topUsers: Member[];
+}
+
+/*. PRODUCT PAGE STATE */
+
+
+/*. ORDER PAGE STATE */
