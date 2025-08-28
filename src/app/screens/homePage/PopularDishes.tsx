b7514fc0ced9c81e28f27472767cad3f490615d8
valijon1989 <valijon.jon89@gmail.com>
2025-08-29 01:18:46 +0900

    B: fix: retteive fresh menu and modify NewDishes component

--- src/app/screens/homePage/PopularDishes.tsx
+++ src/app/screens/homePage/PopularDishes.tsx
@@ -31,8 +31,6 @@ const popularDishesRetriever = createSelector(
 export default function PopularDishes() {
  const { popularDishes } = useSelector(popularDishesRetriever);
 
-     console.log("popularDishes:", popularDishes);
-
     return (
         <div className={"popular-dishesh-frame"}>
             <Container>
@@ -40,10 +38,10 @@ export default function PopularDishes() {
                     <Box className={"category-title"}>Popular Dishes</Box>
                     <Stack className={"cards-frame"}>
                          {popularDishes.length !== 0 ? (
-                        popularDishes.map((ele: Product) => {
-                            const imagePath = `${serverApi}/${ele.productImages[0]}`;
+                        popularDishes.map((product: Product) => {
+                            const imagePath = `${serverApi}/${product.productImages[0]}`;
                             return (
-                      <CssVarsProvider key={ele._id}>
+                      <CssVarsProvider key={product._id}>
                          <Card className={"card"} sx={{ minHeight: '280px', width: 320 }}>
                           <CardCover >
                            <img src={imagePath} alt=""/>
@@ -58,7 +56,7 @@ export default function PopularDishes() {
                                         textColor="#fff"
                                         mb={1}
                              >
-                                 {ele.productName}
+                                 {product.productName}
                              </Typography>
                           <Typography
                             sx={{
@@ -68,7 +66,7 @@ export default function PopularDishes() {
                             display: "flex",
                            }}
                           >
-                        {ele.productViews}
+                        {product.productViews}
                        <VisibilityIcon sx={{ fontSize: 25, marginLeft: "5px"}}
                         />
                      </Typography>
@@ -88,7 +86,7 @@ export default function PopularDishes() {
                           startDecorator={<DescriptionOutlinedIcon />}
                           textColor="neutral.300"
                            >
-                           {ele.productDesc}
+                           {product.productDesc}
                         </Typography>
                       </CardOverflow>
                      </Card>
--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,29 +5,28 @@ import ActiveUsers from "./ActiveUsers";
 import PopularDishes from "./PopularDishes";
 import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
-import "../../../css/home.css";
-
-
 import { useDispatch } from "react-redux";
 import { Dispatch }  from "@reduxjs/toolkit";
-import { setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import "../../../css/home.css";
+
 
 /** REDUX SKICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+    setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 });
 
 
 export default function HomePage() {
-  const { setPopularDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
 
 
   useEffect(() => {
   //Backenddan popular disheslarni olib kelish va Redux store ga set qilish
-
   const product = new ProductService();
   product
     .getProducts({
@@ -40,7 +39,20 @@ export default function HomePage() {
       console.log("data passed here:", data);
       setPopularDishes(data);
     })
-    .catch();
+    .catch((err) => console.log(err));
+
+product
+    .getProducts({
+      page: 1,
+      limit: 4,
+      order: "createdAt",
+      // productCollection: ProductCollection.DISH,
+    })
+    .then((data) => {
+      setNewDishes(data);
+    })
+    .catch((err) => console.log(err));
+
   }, []);
 
   return (
