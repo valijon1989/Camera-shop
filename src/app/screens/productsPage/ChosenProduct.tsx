33977a2bb1d53f2c3c7cb68f200eb53105e3dfd1
valijon1989 <valijon.jon89@gmail.com>
2025-08-31 03:16:15 +0900

    B: fix: modify ChosenProduct componenet logic

--- src/app/screens/productsPage/ChosenProduct.tsx
+++ src/app/screens/productsPage/ChosenProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, Stack, Box } from "@mui/material";
 import { Swiper, SwiperSlide } from "swiper/react";
 import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
@@ -16,12 +16,17 @@ import { setRestaurant, setChosenProduct } from "./slice";
 import { Product } from "../../../lib/types/product";
 import { createSelector } from "reselect";
 import { retrieveChosenProduct, retrieveRestaurant } from "./selector";
+import { useParams } from "react-router-dom";
+import ProductService from "../../services/ProductService";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
+import { serverApi } from "../../../lib/config";
 
 
 /** REDUX SKICE & SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
-    setRestaurant: (data: Product[]) => dispatch(setRestaurant(data)),
-    setChosenProduct: (data: Product[]) => dispatch(setChosenProduct(data)),
+    setRestaurant: (data: Member) => dispatch(setRestaurant(data)),
+    setChosenProduct: (data: Product) => dispatch(setChosenProduct(data)),
 });
 const chosenProductRetriever = createSelector(retrieveChosenProduct,(chosenProduct) => ({
    chosenProduct,
@@ -33,6 +38,26 @@ const chosenProductRetriever = createSelector(retrieveChosenProduct,(chosenProdu
 
 
 export default function ChosenProduct() {
+  const { productId } = useParams<{ productId: string }>();
+  const { setRestaurant, setChosenProduct } = actionDispatch(useDispatch());
+  const { chosenProduct } = useSelector(chosenProductRetriever);
+  const { restaurant } = useSelector(restaurantRetriever);
+
+  useEffect(() => {
+    const product = new ProductService();
+    product
+    .getProduct(productId)
+    .then(data => setChosenProduct(data))
+    .catch((err) => console.log( err));
+
+    const member = new MemberService();
+    member
+    .getRestaurant()
+    .then((data) => setRestaurant(data))
+    .catch((err) => console.log( err));
+  }, []);
+
+  if (!chosenProduct)  return null; 
   return (
     <div className={"chosen-product"}>
       <Box className={"title"}>Product Detail</Box>
@@ -45,12 +70,17 @@ export default function ChosenProduct() {
             modules={[FreeMode, Navigation, Thumbs]}
             className="swiper-area"
           >
-            {["/img/cutlet.webp", "/img/kebab-fresh.webp"].map(
+            {chosenProduct?.productImages.map(
               (ele: string, index: number) => {
+                const ImagePath = `${serverApi}/${ele}`;
                 return (
                   <SwiperSlide key={index}>
-                    <img className="slider-image" src={ele} />
-                  </SwiperSlide>
+                   <img
+                    className="slider-image"
+                    src={ImagePath}
+                    alt={chosenProduct?.productName || "Mahsulot rasmi"}
+                   />
+                 </SwiperSlide>
                 );
               }
             )}
@@ -58,22 +88,27 @@ export default function ChosenProduct() {
         </Stack>
         <Stack className={"chosen-product-info"}>
           <Box className={"info-box"}>
-            <strong className={"product-name"}>Kebab</strong>
-            <span className={"resto-name"}>Burak</span>
+            <strong className={"product-name"}>{chosenProduct?.productName}</strong>
+            <span className={"resto-name"}>{restaurant?.memberNick}</span>
+             <span className={"resto-name"}>{restaurant?.memberPhone}</span>
             <Box className={"rating-box"}>
               <Rating name="half-rating" defaultValue={2.5} precision={0.5} />
               <div className={"evaluation-box"}>
                 <div className={"product-view"}>
                   <RemoveRedEyeIcon sx={{ mr: "10px" }} />
-                  <span>20</span>
+                  <span>{chosenProduct?.productViews}</span>
                 </div>
               </div>
             </Box>
-            <p className={"product-desc"}>Our best product</p>
+            <p className={"product-desc"}>
+              {chosenProduct?.productDesc
+            ? chosenProduct?.productDesc 
+            : "No Description"}
+            </p>
             <Divider height="1" width="100%" bg="#000000" />
             <div className={"product-price"}>
               <span>Price:</span>
-              <span>$12</span>
+              <span>{chosenProduct?.productPrice}</span>
             </div>
             <div className={"button-box"}>
               <Button variant="contained">Add To Basket</Button>
--- src/app/services/ProductService.ts
+++ src/app/services/ProductService.ts
@@ -28,6 +28,20 @@ class ProductService {
         }
      }
 
+     public async getProduct(productId: string): Promise<Product> {
+        try {
+            const url = `${this.path}/product/${productId}`;
+            const result = await axios.get(url, {withCredentials: true});
+            
+            console.log("getProduct:", result);
+
+
+            return result.data;
+        } catch (err) {
+            console.log("Error getProduct:", err);
+            throw err;
+        }
+     }
 }
 
 
