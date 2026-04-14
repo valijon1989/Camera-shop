c861564be24168f15fff64a7e509d8832d679ffd
valijon1989 <valijon.jon89@gmail.com>
2026-04-14 22:11:00 +0900

    chore: update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -e
 
-# PRODUCTION
 git reset --hard
 git checkout master
 git pull origin master
 
-npm i yarn -g
+npm i -g yarn
 yarn global add serve
 yarn
-yarn run build
+yarn build
 
-pm2 start "yarn run start:prod" --name CAMERA_SHOP
+if pm2 describe CAMERA_SHOP >/dev/null 2>&1; then
+  pm2 restart CAMERA_SHOP
+else
+  pm2 start "serve -s build -l 3010" --name CAMERA_SHOP
+fi
+
+pm2 save
